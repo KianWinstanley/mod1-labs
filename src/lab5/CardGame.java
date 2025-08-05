73d3228ee9cbe20b3fdd46d9b6cba2b96dc40fc9
Kian Winstanley <kian.winstanley@airelogic.com>
2025-08-05 15:20:29 +0100

    lab 01d extra completed

--- src/lab5/CardGame.java
+++ src/lab5/CardGame.java
@@ -16,6 +16,8 @@ public class CardGame {
 	public static void GetCards() {
 		String[] cards = {"Ace Spades","2 Spades","3 Spades","4 Spades","5 Spades","6 Spades","7 Spades","8 Spades","9 Spades","10 Spades","Jack Spades","Queen Spades","King Spades","Ace Hearts","2 Hearts","3 Hearts","4 Hearts","5 Hearts","6 Hearts","7 Hearts","8 Hearts","9 Hearts","10 Hearts","Jack Hearts","Queen Hearts","King Hearts","Ace Clubs","2 Clubs","3 Clubs","4 Clubs","5 Clubs","6 Clubs","7 Clubs","8 Clubs","9 Clubs","10 Clubs","Jack Clubs","Queen Clubs","King Clubs","Ace Diamonds","2 Diamonds","3 Diamonds","4 Diamonds","5 Diamonds","6 Diamonds","7 Diamonds","8 Diamonds","9 Diamonds","10 Diamonds","Jack Diamonds","Queen Diamonds","King Diamonds"}; 
 		
+		shuffleArray(cards);
+		
 		Queue<String> playerHandQueue = new LinkedList<>();
 		Queue<String> houseHandQueue = new LinkedList<>();
 		
@@ -95,5 +97,18 @@ public class CardGame {
 		}
 		return handTotal;
 	}
+	
+	public static void shuffleArray(String[] array) {
+        Random rand = new Random();
+        for (int i = array.length - 1; i > 0; i--) {
+            int j = rand.nextInt(i + 1); // random index from 0 to i
+
+
+            // Swap array[i] with array[j]
+            String temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 }
 
