49016d888d0fe3390fcbff77674eea1a9b831161
Kian Winstanley <kian.winstanley@airelogic.com>
2025-08-05 15:22:02 +0100

    lab 01d extra completed

--- src/lab5/CardGame.java
+++ src/lab5/CardGame.java
@@ -11,6 +11,18 @@ public class CardGame {
 	
 	public static void main(String[] args) {
 		GetCards();
+	}
+	
+	public static void GetCards() {
+		String[] cards = {"Ace Spades","2 Spades","3 Spades","4 Spades","5 Spades","6 Spades","7 Spades","8 Spades","9 Spades","10 Spades","Jack Spades","Queen Spades","King Spades","Ace Hearts","2 Hearts","3 Hearts","4 Hearts","5 Hearts","6 Hearts","7 Hearts","8 Hearts","9 Hearts","10 Hearts","Jack Hearts","Queen Hearts","King Hearts","Ace Clubs","2 Clubs","3 Clubs","4 Clubs","5 Clubs","6 Clubs","7 Clubs","8 Clubs","9 Clubs","10 Clubs","Jack Clubs","Queen Clubs","King Clubs","Ace Diamonds","2 Diamonds","3 Diamonds","4 Diamonds","5 Diamonds","6 Diamonds","7 Diamonds","8 Diamonds","9 Diamonds","10 Diamonds","Jack Diamonds","Queen Diamonds","King Diamonds"}; 
+		
+		shuffleArray(cards);
+		
+		Queue<String> playerHandQueue = new LinkedList<>();
+		Queue<String> houseHandQueue = new LinkedList<>();
+		
+		calculateHand(cards, playerHandQueue);
+		calculateHand(cards, houseHandQueue);
 		
 		int playerHandTotal = 0;
 		int houseHandTotal = 0;
@@ -29,18 +41,6 @@ public class CardGame {
 		else {
 			System.out.println("The Player has won with a total of " + playerHandTotal);
 		}
-	}
-	
-	public static void GetCards() {
-		String[] cards = {"Ace Spades","2 Spades","3 Spades","4 Spades","5 Spades","6 Spades","7 Spades","8 Spades","9 Spades","10 Spades","Jack Spades","Queen Spades","King Spades","Ace Hearts","2 Hearts","3 Hearts","4 Hearts","5 Hearts","6 Hearts","7 Hearts","8 Hearts","9 Hearts","10 Hearts","Jack Hearts","Queen Hearts","King Hearts","Ace Clubs","2 Clubs","3 Clubs","4 Clubs","5 Clubs","6 Clubs","7 Clubs","8 Clubs","9 Clubs","10 Clubs","Jack Clubs","Queen Clubs","King Clubs","Ace Diamonds","2 Diamonds","3 Diamonds","4 Diamonds","5 Diamonds","6 Diamonds","7 Diamonds","8 Diamonds","9 Diamonds","10 Diamonds","Jack Diamonds","Queen Diamonds","King Diamonds"}; 
-		
-		shuffleArray(cards);
-		
-		Queue<String> playerHandQueue = new LinkedList<>();
-		Queue<String> houseHandQueue = new LinkedList<>();
-		
-		calculateHand(cards, playerHandQueue);
-		calculateHand(cards, houseHandQueue);
 	}
 
 	private static void calculateHand(String[] cards, Queue<String> handQueue) {
